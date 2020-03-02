dc28aa0142970164721ef0d8ea3fa317fb972dbf
jiangxiaqiu <jiangxiaqiu@yy.com>
2020-03-02 23:28:18 +0800

    ci: update ci workflow

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 ./gradlew buildPlugin
 cd ./build/idea-sandbox/plugins/
-zip -o -r '/Users/jiangxq 1/plugin_output/code_toolbox/CodeToolBox.zip' './code_toolbox'
+zip -o -r './CodeToolBox.zip' './code_toolbox'
